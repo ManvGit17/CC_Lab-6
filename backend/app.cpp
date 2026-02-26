115dcb71187741954aa1798c923847d4abbe28da
ManvGit17 <manitvurs2005@gmail.com>
2026-02-26 21:33:20 +0530

    Fix binding and error handling

--- backend/app.cpp
+++ backend/app.cpp
@@ -5,20 +5,40 @@
 
 int main() {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("socket failed");
+        return 1;
+    }
+
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
     sockaddr_in address;
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_addr.s_addr = INADDR_ANY;  // bind to 0.0.0.0
     address.sin_port = htons(8080);
 
-    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
-    listen(server_fd, 10);
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        return 1;
+    }
+
+    if (listen(server_fd, 10) < 0) {
+        perror("listen failed");
+        return 1;
+    }
 
     char hostname[256];
     gethostname(hostname, sizeof(hostname));
 
+    std::cout << "Server running on 0.0.0.0:8080\n";
+
     while (true) {
         int client_socket = accept(server_fd, NULL, NULL);
+        if (client_socket < 0) {
+            perror("accept failed");
+            continue;
+        }
 
         std::string response =
             "HTTP/1.1 200 OK\r\n"
